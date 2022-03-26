cc22920779294566abb205a2feadbfd1c73846a7
Thamires Lourenço <thami.lourenco@outlook.com>
2022-03-26 18:12:03 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -12,10 +12,42 @@ function renderWithRouter(component) {
   });
 }
 
+const mockFavoritePokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+
+  }];
+
 describe('Teste o componente Favorite Pokemons', () => {
   it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
     renderWithRouter(<FavoritePokemons />);
     const paragraphNotFavorite = screen.getByText(/No favorite pokemon found/i);
     expect(paragraphNotFavorite).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockFavoritePokemons } />);
+    const cards = screen.getAllByTestId('pokemon-name');
+    expect(cards).toHaveLength(2);
   });
 });

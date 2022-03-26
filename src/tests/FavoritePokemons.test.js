2aaf91fea1e7a833bdd19b32cd1dea9750e12db4
Thamires Lourenço <thami.lourenco@outlook.com>
2022-03-26 17:33:16 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+function renderWithRouter(component) {
+  const saveRouter = createMemoryHistory();
+
+  return ({
+    ...render(<Router history={ saveRouter }>{component}</Router>), saveRouter,
+  });
+}
+
+describe('Teste o componente Favorite Pokemons', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const paragraphNotFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(paragraphNotFavorite).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+function renderWithRouter(component) {
+  const saveRouter = createMemoryHistory();
+
+  return ({
+    ...render(<Router history={ saveRouter }>{component}</Router>), saveRouter,
+  });
+}
+
+describe('Testar o componente Not Found', () => {
+  it('Teste se página contém um h2 com o texto Page requested not found ', () => {
+    renderWithRouter(<NotFound />);
+    const headingNotFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const giphyImage = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(giphyImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
+// https://testing-library.com/docs/queries/byalttext/

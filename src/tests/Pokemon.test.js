c392dded07429f0705bf1a2347bcab627d635ea4
Thamires Lourenço <thami.lourenco@outlook.com>
2022-03-30 13:22:06 -0300

    requisito 6 incompleto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import Pokemon from '../components/Pokemon';
+
+function renderWithRouter(component) {
+  const saveRouter = createMemoryHistory();
+
+  return ({
+    ...render(<Router history={ saveRouter }>{component}</Router>), saveRouter,
+  });
+}
+
+const mockPokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+};
+
+describe('Teste se é renderizado card com as informações de determinado pokémon', () => {
+  it('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<Pokemon pokemon={ mockPokemon } />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent(/Pikachu/i);
+  });
+
+  it('O tipo correto do pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<Pokemon pokemon={ mockPokemon } />);
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent(/Electric/i);
+  });
+
+  it('O peso médio do pokémon deve ser exibido com um texto', () => {
+    renderWithRouter(<Pokemon pokemon={ mockPokemon } />);
+
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight).toHaveTextContent(/6.0/i);
+  });
+});
